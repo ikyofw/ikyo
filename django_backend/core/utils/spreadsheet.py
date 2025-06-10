1899b24d54170305ebd3068729b51724c2ab628c
Muiz <muiz@163.com>
2025-06-10 11:16:51 +0800

    Bugfix: django_backend/core/utils/spreadsheet.py

--- django_backend/core/utils/spreadsheet.py
+++ django_backend/core/utils/spreadsheet.py
@@ -3,15 +3,21 @@ import logging
 import math
 import os
 import pathlib
+import re
 import string
 import traceback
+from plistlib import InvalidFileException
 
 import django.core.files.uploadedfile as djangoUploadedfile
 import pandas as pd
+from openpyxl import load_workbook
+from openpyxl.cell.cell import MergedCell
+from openpyxl.utils import get_column_letter, range_boundaries
+from openpyxl.worksheet.worksheet import Worksheet
+
 from core.core.exception import IkValidateException
 from core.core.lang import Boolean2
 from core.utils.langUtils import isNullBlank
-from openpyxl import load_workbook
 
 logger = logging.getLogger('ikyo')
 
@@ -66,6 +72,10 @@ class SpreadsheetParser:
     '''
         End of Table
     '''
+    END_FLAG = 'EOD'
+    '''
+        END of Component
+    '''
 
     def __init__(self, excelFile, ignoreBlankLines=True, tableNames=None):
         self.__firstDataColumnIndex = DATA_START_COLUMN_INDEX - 1  # starts from 0 . the value is (3-1)=2
@@ -82,7 +92,7 @@ class SpreadsheetParser:
             self.__file = excelFile
         else:
             raise Exception('[' + excelFile + '] file does not exist.')
-        
+
         with pd.ExcelFile(excelFile) as f:
             sheetData = []
             for sheetName in f.sheet_names:
@@ -99,7 +109,7 @@ class SpreadsheetParser:
                     row = data[i]
                     prmName = row[0]
                     if prmName is not None and len(prmName) > 0:
-                        if prmName.upper() == 'EOD':
+                        if prmName.upper() == self.END_FLAG:
                             break
                         elif prmName[0] != '>':  # ignore output parameters. e.g. ">projectName"
                             v = self.__readPrm(prmName, data, i)
@@ -130,7 +140,7 @@ class SpreadsheetParser:
                     row = row[self.__firstDataColumnIndex:]
                     endTableFlag = row[0]
                     if endTableFlag is not None and type(endTableFlag) == str \
-                            and endTableFlag == SpreadsheetParser.TABLE_END_FLAG:
+                            and endTableFlag == self.TABLE_END_FLAG:
                         break
                     rowCellData = row[0:totalTableColumns].tolist()
                     if self.__ignoreBlankLines:
@@ -155,7 +165,7 @@ class SpreadsheetParser:
             prmName, tableFlag = row[0], row[2]
             if not isNullBlank(prmName):
                 return False
-            if tableFlag == SpreadsheetParser.TABLE_END_FLAG:
+            if tableFlag == self.TABLE_END_FLAG:
                 return True
         return False
 
@@ -223,7 +233,10 @@ class SpreadsheetWriter:
     def write(self) -> Boolean2:
         wb = None
         try:
-            wb = load_workbook(self.__templateFile)
+            try:
+                wb = load_workbook(self.__templateFile)
+            except InvalidFileException as e:
+                raise IkValidateException(f"Template file format error: {self._templateFile}\n{e}")
             if len(self.__parameters) > 0:
                 self.__write(wb)
             if os.path.isfile(self.__outputFile):
@@ -269,7 +282,7 @@ class SpreadsheetWriter:
                                 nameSheetDict[parameter_name].append(st)
         for name, value in self.__parameters.items():
             if name not in nameSheetDict.keys():
-                print('Parameter [%s] is not found in the tempate file [%s]' % (name, self.__templateFile))
+                print('Parameter [%s] is not found in the template file [%s]' % (name, self.__templateFile))
             sheet_objects = nameSheetDict.get(name, None)
             for st in sheet_objects:
                 data = value
@@ -281,19 +294,40 @@ class SpreadsheetWriter:
                     if sheetName is not None:
                         st = wb[sheetName]
                         if st is None:
-                            raise IkValidateException('Sheet [%s] does not exist. Plesae check.' % sheetName)
+                            raise IkValidateException('Sheet [%s] does not exist. Please check.' % sheetName)
                     data = value.get('data', None)
                     columnIndex = value.get('columnIndex', DATA_START_COLUMN_INDEX)
                     if columnIndex is not None and type(columnIndex) == str:
                         columnIndex = columnName2Index(columnIndex)
                     rowIndex = value.get('rowIndex', None)
                 if st is None:
-                    raise IkValidateException('Parameter name [%s] does not exist. Plesae check.' % name)
+                    raise IkValidateException('Parameter name [%s] does not exist. Please check.' % name)
 
                 if self.__isSingleValue(st, name, data):
                     self.__writeSingleValue(st, name, data, columnIndex=columnIndex, rowIndex=rowIndex)
                 else:
-                    self.__writeTableValue(st, name, data, columnIndex=columnIndex, rowIndex=rowIndex)
+                    table_is_last_area = self.__table_is_last_area(st, name)
+                    self.__writeTableValue(st, name, data, columnIndex=columnIndex, rowIndex=rowIndex, is_last_area=table_is_last_area)
+
+    def __table_is_last_area(self, st, name) -> bool:
+        """ Check Is the name component the last component
+        """
+        start_row = None
+        # find table row
+        for cell in st['A']:
+            if cell.value == name:
+                start_row = cell.row
+                break
+        values_below = []
+        row = start_row + 1
+        while row <= st.max_row:
+            value = st.cell(row=row, column=cell.column).value
+            if value is not None:
+                if len(values_below) == 0 and value == SpreadsheetParser.END_FLAG:
+                    return True
+                values_below.append(value)
+            row += 1
+        return False if len(values_below) > 0 else True
 
     def __isSingleValue(self, st, name, value) -> bool:
         return type(value) != list  # None can be single value or table value
@@ -315,22 +349,50 @@ class SpreadsheetWriter:
         rowNo = rowIndex if rowIndex is not None else self.__getNameRowIndex(st, name)
         st[columnIndex2Name(columnIndex) + str(rowNo)] = value
 
-    def __writeTableValue(self, st, name, values, columnIndex=DATA_START_COLUMN_INDEX, rowIndex=None):
+    def __is_merged_but_not_top_left(self, sheet, cell):
+        return isinstance(cell, MergedCell)
+
+    def __find_next_writable_cell(self, sheet, row_idx, col_idx):
+        max_col = sheet.max_column
+        for offset in range(1, max_col - col_idx + 1):
+            try_col = col_idx + offset
+            cell = sheet.cell(row=row_idx, column=try_col)
+            if not isinstance(cell, MergedCell):
+                return cell
+        return None
+
+    def __writeTableValue(self, st, name, values, columnIndex=DATA_START_COLUMN_INDEX, rowIndex=None, is_last_area=False):
         if values is None or len(values) == 0:
             return
         v2 = [(v if type(v) == list else [v]) for v in values]
 
         lastTitleRow = rowIndex - 1 if rowIndex is not None else self.__getNameRowIndex(st, name)
 
+        # get header merged cells
+        header_merges = []
+        for merged_range in st.merged_cells.ranges:
+            min_col, min_row, max_col, max_row = merged_range.bounds
+            if min_row == max_row == lastTitleRow:
+                header_merges.append((min_col, max_col))
         tableColumnTitles = []
         firstRowCells = []
+        merged_cell_num = 0
         for colIndex in range(columnIndex, len(st[lastTitleRow]) + 1):
-            title = st[columnIndex2Name(colIndex) + str(lastTitleRow)].value
-            if title is None or title == '':
-                break
+            col_idx = colIndex + merged_cell_num
+            cell = st[columnIndex2Name(col_idx) + str(lastTitleRow)]
+            if self.__is_merged_but_not_top_left(st, cell):
+                new_cell = self.__find_next_writable_cell(st, lastTitleRow, col_idx)
+                if new_cell:
+                    tableColumnTitles.append(new_cell.value)
+                    firstRowCells.append(st[columnIndex2Name(new_cell.column) + str(lastTitleRow + 1)])
+                    merged_cell_num += new_cell.column - cell.column
+                else:
+                    break
             else:
-                tableColumnTitles.append(title)
-                firstRowCells.append(st[columnIndex2Name(colIndex) + str(lastTitleRow + 1)])
+                if cell.value is None or cell.value == '':
+                    break
+                tableColumnTitles.append(cell.value)
+                firstRowCells.append(st[columnIndex2Name(col_idx) + str(lastTitleRow + 1)])
         rowIndex = lastTitleRow + 1
 
         # clean table data if exists
@@ -338,34 +400,140 @@ class SpreadsheetWriter:
         totalEmptyRows = 0
         while True:
             tableEndFlag = st[columnIndex2Name(columnIndex) + str(rowNo)].value
-            if tableEndFlag is not None and tableEndFlag == SpreadsheetParser.TABLE_END_FLAG:
+            if tableEndFlag == SpreadsheetParser.TABLE_END_FLAG:
                 break
-            for j in range(len(tableColumnTitles)):
-                cellName = columnIndex2Name(columnIndex + j) + str(rowNo)
-                st[cellName] = None  # clean
+            for first_row_cell in firstRowCells:
+                cell = st.cell(row=rowIndex, column=first_row_cell.column)
+                cell.value = None
             rowNo += 1
             if rowNo > st.max_row or rowNo > 1048576:
                 break
             totalEmptyRows += 1
 
+        required_rows = len(v2) - totalEmptyRows  # need insert rows number
+
+        # Completely move the content below the table (including styles and merged cells)
+        # Noted: must be processed before inserting data into the table
+        if required_rows > 0 and not is_last_area:
+            self.__shift_tail_area(st, move_start_row=rowIndex + totalEmptyRows, move_count=required_rows)  # Move down all areas after the table by required_rows rows
+            self.__shift_print_area(st, row_offset=required_rows)  # Move down the print area by required_rows rows
+
         for i in range(len(v2)):
             rowData = v2[i]
-            if i > 0:   # tempate table has a blank row
+            if len(rowData) != len(tableColumnTitles):
+                logger.error("Table %s Row %s data length [%s] does not match table column length [%s]." % (name, i + 1, len(rowData), len(tableColumnTitles)))
+                raise IkValidateException("Table %s Row %s data length does not match table column length." % (name, i + 1))
+            if i > 0:   # template table has a blank row
                 rowIndex += 1
                 if totalEmptyRows == 1:
-                    st.insert_rows(rowIndex)
+                    # is not last table, insert at __shift_tail_area method
+                    if is_last_area:
+                        st.insert_rows(rowIndex)
+                    # copy style and cells merge
+                    self.__hand_row_style_merges(st, rowIndex - 1, rowIndex, header_merges)
                 else:
                     totalEmptyRows -= 1
-            for j in range(len(rowData)):
-                cellName = columnIndex2Name(columnIndex + j) + str(rowIndex)
-                try:
-                    st[cellName] = rowData[j]
-                except Exception as e:
-                    msg = 'Fill in cell [%s].[%s] failed: %s' % (st.title, cellName, str(e))
-                    logger.error(msg)
-                    raise e
+
+            for j, first_row_cell in enumerate(firstRowCells):
+                cell_data = rowData[j]
+                cell = st.cell(row=rowIndex, column=first_row_cell.column)
+                cell.value = cell_data
             if i > 0:    # update the cell styles (starts from the 2nd row)
-                for j in range(len(firstRowCells)):
-                    toCell = firstRowCells[j]
-                    cellName = columnIndex2Name(columnIndex + j) + str(rowIndex)
-                    copyCellStyle(st[cellName], toCell)
+                for j, first_row_cell in enumerate(firstRowCells):
+                    cellName = columnIndex2Name(first_row_cell.column) + str(rowIndex)
+                    copyCellStyle(st[cellName], first_row_cell)
+
+    def __hand_row_style_merges(self, st, src_row_idx, dst_row_idx, header_merges):
+        # copy style
+        for col in range(1, st.max_column + 1):
+            src_cell = st.cell(row=src_row_idx, column=col)
+            dst_cell = st.cell(row=dst_row_idx, column=col)
+            copyCellStyle(dst_cell, src_cell)
+        # copy merge cells
+        for min_col, max_col in header_merges:
+            merge_range = f"{get_column_letter(min_col)}{dst_row_idx}:{get_column_letter(max_col)}{dst_row_idx}"
+            st.merge_cells(merge_range)
+
+    def __shift_tail_area(self, st: Worksheet, move_start_row: int, move_count: int):
+        """ Move down the specified area by the specified row
+
+        Args:
+            st (Worksheet): sheet
+            move_start_row (int): start row number
+            move_count (int): rows offset number
+        """
+        max_col = st.max_column
+        max_row = st.max_row
+
+        # Save area content and style
+        temp_rows = []
+        for r in range(max_row, move_start_row - 1, -1):  # Handle from bottom to top to avoid coverage
+            row_data = []
+            for c in range(1, max_col + 1):
+                cell = st.cell(row=r, column=c)
+                row_data.append((cell.value, copy.copy(cell._style)))
+            temp_rows.insert(0, row_data)  # Insert to the front and maintain order
+
+        # Save merged cells
+        merge_to_shift = []
+        for merge in list(st.merged_cells.ranges):
+            if merge.min_row >= move_start_row:
+                merge_to_shift.append(merge)
+                st.unmerge_cells(str(merge))
+
+        # insert rows
+        st.insert_rows(move_start_row, amount=move_count)
+
+        # Restore content and style
+        for i, row_data in enumerate(temp_rows):
+            for j, (val, style) in enumerate(row_data):
+                cell = st.cell(row=move_start_row + move_count + i, column=j + 1)
+                cell.value = val
+                cell._style = style
+
+        # Restore merged cells
+        for merge in merge_to_shift:
+            merge.shift(0, move_count)
+            if merge is not None:
+                try:
+                    st.merge_cells(merge.coord)
+                except Exception as e:
+                    logger.warning(f"Skip invalid merged cells {merge.coord} - {e}")
+            else:
+                logger.warning(f"Skip empty merged cells.")
+
+    def __shift_print_area(self, st, row_offset: int):
+        """ print area add rows
+
+        Args:
+            st (_type_): sheet
+            row_offset (int): rws
+
+        Raises:
+            ValueError: exception
+        """
+        if not st.print_area:
+            return  # No print area
+
+        if isinstance(st.print_area, (list, tuple)):  # list. eg. ['$B$2:$S$35']
+            print_area_str = st.print_area[0]
+        elif isinstance(st.print_area, str):  # str. eg. "'output'!$B$2:$S$35"
+            print_area_str = st.print_area
+        else:
+            raise TypeError(f"Unsupported print_area type: {type(st.print_area)}")
+
+        # get $B$2:$S$35 or B2:S35 format
+        match = re.search(r"([$A-Z]+\$?\d+:[$A-Z]+\$?\d+)", print_area_str)
+        if not match:
+            raise ValueError(f"Invalid print_area format: {print_area_str}")
+
+        coord = match.group(1).replace('$', '')
+        min_col, min_row, max_col, max_row = range_boundaries(coord)
+        new_range = f"{get_column_letter(min_col)}{min_row}:{get_column_letter(max_col)}{max_row + row_offset}"
+
+        sheet_match = re.match(r"(.*?)!?\$?[A-Z]+\$?\d+:\$?[A-Z]+\$?\d+", print_area_str)
+        if sheet_match and "!" in sheet_match.group(0):
+            sheet_name = sheet_match.group(1)
+            new_range = f"{sheet_name}!{new_range}"
+
+        st.print_area = new_range
